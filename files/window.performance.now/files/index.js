61b063cbeb6488e9f3e6c01cf4ea6c847219f69e
Alexander Abashkin <monolithed@gmail.com>
2015-05-17 19:49:22 +0300

    git -> http

--- files/window.performance.now/files/index.js
+++ files/window.performance.now/files/index.js
@@ -6,14 +6,13 @@
     }
 
     if ('now' in global.performance == false) {
-
         var nowOffset = Date.now();
 
         if (global.performance.timing && global.performance.timing.navigationStart) {
             nowOffset = global.performance.timing.navigationStart
         }
 
-        global.performance.now = function now () {
+        global.performance.now = function () {
             return Date.now() - nowOffset;
         }
     }

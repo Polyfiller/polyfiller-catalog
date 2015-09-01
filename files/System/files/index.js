64b06150692eacfaf10b51c4aaa538cea7352732
Alexander Abashkin <monolithed@gmail.com>
2015-09-01 18:48:22 +0300

    new modules: System, Number.prototype.toLocaleString

---
+++ files/System/files/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import path from 'path';
+import resolve from 'resolve';
+
+let file = resolve.sync('es6-module-loader');
+    file = path.dirname(file);
+    file = path.join(file, 'dist/es6-module-loader.js');
+
+export default file;
